8ca7afec4e1bd2259f787157570d14d378aef639
Carlos Noel <carlosnoelg3@outlook.com>
2018-12-27 10:53:16 -0600

    First Commit with pageObject Pattern

---
+++ Training QA Automation/Training QA Automation/Framework/Actions.cs
@@ -0,0 +1,15 @@
+﻿namespace Training_QA_Automation.Framework.PageObjects
+{
+    public class Actions
+    {
+        public void clickOn()
+        {
+
+        }
+
+        public void Type()
+        {
+
+        }
+    }
+}
---
+++ Training QA Automation/Training QA Automation/Framework/PageObjects/RegistrationPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Training_QA_Automation.Framework.PageObjects
+{
+    public class RegistrationPage
+    {
+        private IWebDriver driver;
+        private string url = "https://www.taxact.com/";
+
+        public RegistrationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        /// id =  sign-in-desk
+        [FindsBy(How = How.Id, Using = "sign-in-desk")]
+        public IWebElement SignInButton { get; set; }
+        
+        /// create an account = a.btn (CssSelector)
+        [FindsBy(How = How.CssSelector, Using = "a.btn")]
+        public IWebElement CreateNewAccount { get; set; }
+
+        /// Method to navigate into the website
+        public void GoToPage(string queryParam ="")
+        {
+            driver.Navigate().GoToUrl(url + "/");
+        }
+        
+
+    }
+}
