63e7f69b1fd70141b49a2d73c665e455bf83b935
CarlosNoelJ <CarlosNoelJ@users.noreply.github.com>
2018-12-28 13:57:13 -0600

    SauceLabs Configuration (#3)
    * SauceLabs Configuration
    
    * .Gitignore fixed

--- Training QA Automation/Training QA Automation/Framework/PageObjects/RegistrationPage.cs
+++ Training QA Automation/Training QA Automation/Framework/PageObjects/RegistrationPage.cs
@@ -1,5 +1,9 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.PageObjects;
+using System;
 
 namespace Training_QA_Automation.Framework.PageObjects
 {
@@ -21,13 +25,11 @@ namespace Training_QA_Automation.Framework.PageObjects
         /// create an account = a.btn (CssSelector)
         [FindsBy(How = How.CssSelector, Using = "a.btn")]
         public IWebElement CreateNewAccount { get; set; }
-
+        
         /// Method to navigate into the website
-        public void GoToPage(string queryParam ="")
+        public void GoToPage()
         {
             driver.Navigate().GoToUrl(url + "/");
         }
-        
-
     }
 }
--- Training QA Automation/Training QA Automation/Tests/RegistrationTest.cs
+++ Training QA Automation/Training QA Automation/Tests/RegistrationTest.cs
@@ -1,25 +1,52 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using Training_QA_Automation.Framework.PageObjects;
 
 namespace Training_QA_Automation.Tests
 {
-    public class RegistrationTest
+    [TestClass]
+    public class RegistrationTest : Actions
     {
+        public TestContext TestContext { get; set; }
+        
+        IWebDriver driver;
+        ChromeOptions options = new ChromeOptions();
+        
         [TestInitialize]
         public void Initialize()
         {
+            var sauceUserName = Environment.GetEnvironmentVariable("CarlosNoelJ", EnvironmentVariableTarget.User);
+            var sauceAccessKey = Environment.GetEnvironmentVariable("5a765391-c29c-4b1f-9000-0b5c644da8e0", EnvironmentVariableTarget.User);
 
+            options.AddAdditionalCapability(CapabilityType.Version, "latest", true);
+            options.AddAdditionalCapability(CapabilityType.Platform, "Windows 10", true);
+            options.AddAdditionalCapability("username", "CarlosNoelJ", true);
+            options.AddAdditionalCapability("accessKey", "5a765391-c29c-4b1f-9000-0b5c644da8e0", true);
+            options.AddAdditionalCapability("name", TestContext.TestName, true);
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), options.ToCapabilities(), TimeSpan.FromSeconds(600));
         }
 
         [TestMethod]
         public void Test1()
         {
-
+            var registerPage = new RegistrationPage(driver);
+            registerPage.GoToPage();
+            clickOn(driver, registerPage.SignInButton);
+            clickOn(driver, registerPage.CreateNewAccount);
+            Assert.AreEqual("Create a TaxAct account to file your taxes online", driver.Title);
         }
 
         [TestCleanup]
         public void CleanUp()
         {
+            var passed = TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
 
+            ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            if (driver != null)
+                driver.Quit();
         }
     }
 }
